d903b7db9953ecf0dd5c654f7d3a610fe6a752bf
siddhant tripathy <siddhant.tripathy7@gmail.com>
2022-05-09 10:51:56 +0530

    added work file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express")
 
 const app = express()
 let items = []
+let workItem = []
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
@@ -15,13 +16,26 @@ app.get ("/",function(req,res){
 
         month: "long"
     }
+
     let day = date.toLocaleDateString("en-US", options)
-    res.render("list", {dayOfTheWeek: day, items: items} )
+    res.render("list", {title: day, items: items} )
 })
+app.get("/work", function(req,res){
+    res.render("list",{title:"Work day", items: workItem})
+
+})
+
 app.post("/", function(req,res){
     let item = req.body.newItem
-items.push(item)
+    console.log(req.body.list)
+    if(req.body.list === "Work"){
+    workItem.push(item)
+    res.redirect("/work")
+    } else {
+        items.push(item)
 res.redirect("/")
+    }
+
 })
 app.listen (3000,function(req,res){
     console.log("hello cutieeeeeee")
