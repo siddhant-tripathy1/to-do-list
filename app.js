1a73b183703a798350d262a4fe8c1169be183812
siddhant tripathy <siddhant.tripathy7@gmail.com>
2022-05-21 21:25:19 +0530

    added database feature through mongoose

--- app.js
+++ app.js
@@ -3,26 +3,17 @@ const express = require("express")
 const mongoose = require("mongoose")
 mongoose.connect("mongodb://localhost:27017/todolistDB" )
 const taskSchema = {
-    name: String, 
+	name: String, 
 }
 const Task = mongoose.model("item", taskSchema)
 const Task_1 = new Task ({
-    name : "read books "
+	name : "read books "
 })
 const Task_2 = new Task ({
-    name : "Eat clean "
+	name : "Eat clean "
 })
 const Task_3 = new Task ({
-    name : "Pump iron  "
-})
-const startingTask  = [Task_1, Task_2,Task_3]
-Task.insertMany(startingTask, function(err){
-    if (err){
-        console.log(err)
-     } else {
-         console.log("yess")
-     }
-
+	name : "Pump iron  "
 })
 const app = express()
 let workItem = []
@@ -30,35 +21,52 @@ app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.get ("/",function(req,res){
-    let date = new Date()
-    let options = {
-        weekday: "long",
+	
+	Task.find({}, function(err,found){
+		if(found.length === 0){
+			const startingTask  = [Task_1, Task_2,Task_3]
+			Task.insertMany(startingTask, function(err){
+				if (err){
+					console.log(err)
+				 } else {
+					 console.log("yess")
+				 }
+			
+			})
+			res.redirect("/")
+		} else {
+		res.render("list", {title:"Today", items: found} )
 
-        day: "numeric",
-
-        month: "long"
-    }
-
-    let day = date.toLocaleDateString("en-US", options)
-    res.render("list", {title: day, items: items} )
+	}
+	})
+	// 
 })
-app.get("/work", function(req,res){
-    res.render("list",{title:"Work day", items: workItem})
 
+
+app.get("/work", function(req,res){
+	res.render("list",{title:"Work day", items: workItem})
+
+})
+
+app.post("/delete", function(req,res){
+	checkedItemId = req.body.checkbox
+	Task.findByIdAndDelete(checkedItemId, function(err){
+		if(!err){
+                console.log("no error")
+		}
+	})
+	res.redirect("/")
 })
 
 app.post("/", function(req,res){
-    let item = req.body.newItem
-    console.log(req.body.list)
-    if(req.body.list === "Work"){
-    workItem.push(item)
-    res.redirect("/work")
-    } else {
-        items.push(item)
-res.redirect("/")
-    }
+	let newItem = req.body.newItem
+	const item = new Task( {
+		name : newItem
+	})
+	item.save()
+	res.redirect("/")
 
 })
 app.listen (3000,function(req,res){
-    console.log("hello cutieeeeeee")
+	console.log("hello cutieeeeeee")
 })
